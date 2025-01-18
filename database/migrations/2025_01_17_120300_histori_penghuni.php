5313c23b47c96f6c98c58b4e2509b4fb1ed27bc2
Vangzbill <mamadhan017@gmail.com>
2025-01-18 10:03:01 +0700

    feat: add migrations

---
+++ database/migrations/2025_01_17_120300_histori_penghuni.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('histori_penghuni', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('rumah_id')->constrained('rumah');
+            $table->foreignId('penghuni_id')->constrained('penghuni');
+            $table->date('tanggal_mulai');
+            $table->date('tanggal_selesai')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('histori_penghuni');
+    }
+};
